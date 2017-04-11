fcf9f09ee996bacb6f8c3d6e65d45c7e3c6a1385
Hiumathy <Hiumathy@gmail.com>
2017-04-11 18:23:15 -0500

    duplicate bug

--- script.js
+++ script.js
@@ -18,60 +18,97 @@ $(document).ready(function() {
     }
   }
 
-
   // crosses out to do item when clicked
   var checks = document.getElementsByTagName("li");
   for (var i=0; i<checks.length; i++){
     checks[i].onclick = function() {
-      $(this).toggleClass("check");
+      var parent = $(this).parent(); // gets the parent element
+
+      if ($(this).hasClass("check")){
+        var children = parent.children(); // get the whole list
+        for (var j=0; j<children.length; j++) {
+          if ($(children[j]).hasClass("check")) { // if the element has class check
+            $(this).insertBefore(children[j]); // insert the element with the class before the first element the class check
+            break;
+          }
+        }
+      }
+      else
+      parent.append(this);
+
+      $(this).toggleClass("check"); // add a check class
     }
   }
 
 
   var check = document.createTextNode("\u2713\t");
 
-  var toDoList = document.getElementsByClassName("input");
 
-  for (var i=0; i<toDoList.length; i++) {
-    toDoList[i].onclick = function() {
-      $(this).keypress(function(e) {
-        var text = this.value; // gets the value of the input
+  $(".input").click(function() {
+    $(this).keypress(function(e) { //<input></input>
+      var text = this.value; // gets the value of the input
 
-        if (e.which == 13) { // when enter is pressed
-          var toDoItem = document.createElement("li"); // creates <li></li>
-          toDoItem.append(text);                      // <li>text</li>
-          // toDoItem.setAttribute("onclick", "Removeli()");
-          var ul = $(this).next();
-          console.log(ul);
-          ul.append(toDoItem);                // adds to the end of the list
-          this.value = ''; // resets user input
+      if (e.which == 13) { // when enter is pressed
+        var toDoItem = document.createElement("li"); // creates <li></li>
+        toDoItem.append(text);                      // <li>text</li>
 
-          // create x when enter is pressed
-          var span = document.createElement("span");
-          var x = document.createTextNode("\u00D7");
-          span.setAttribute("class", "hide");
-          span.append(x);
-          toDoItem.append(span);
+        var ul = $(this).next(); // <ul></ul>
 
-          for (var i = 0; i < close.length; i++) {
-            // when clicked on
-            close[i].onclick = function() {
-              this.parentElement.style.display = "none"; // does not display the list
-            }
-          }
-
-          // crosses out to do item when clicked
-          for (var i=0; i<checks.length; i++){
-            checks[i].onclick = function() {
-              $(this).toggleClass("check");
-            }
+        var children = ul.children(); // get the whole list
+        var count = 0;
+        for (var i=0; i<children.length; i++) {
+          if ($(children[i]).hasClass("check")) { // if the element has class check
+            $(toDoItem).insertBefore(children[i]); // insert the element with the class before the first element the class check
+            count++;
+            break;
           }
         }
 
-      });
-    }
-  }
+        if (count == 0)
+          ul.append(toDoItem);
 
+        this.value = ''; // resets input value
 
+        
+        // create x when enter is pressed
+        var span = document.createElement("span");
+        var x = document.createTextNode("\u25bd");
+        span.setAttribute("class", "hide");
+        span.append(x);
+        toDoItem.append(span);
 
+        // when x is clicked on
+        for (var i = 0; i < close.length; i++) {
+          // when clicked on
+          close[i].onclick = function() {
+            this.parentElement.style.display = "none"; // does not display the list
+          }
+        }
+
+        // crosses out to do item when clicked
+        for (var i=0; i<checks.length; i++){
+          checks[i].onclick = function() {
+            var parent = $(this).parent(); // gets the parent element
+
+            if ($(this).hasClass("check")){
+              var children = parent.children(); // get the whole list
+              for (var i=0; i<children.length; i++) {
+                if ($(children[i]).hasClass("check")) { // if the element has class check
+                  $(this).insertBefore(children[i]); // insert the element with the class before the first element the class check
+                  break;
+                }
+              }
+            }
+            else
+              parent.append(this);
+
+            $(this).toggleClass("check"); // add a check class
+          }
+        }
+        console.log("1");
+      }
+      console.log("2");
+    });
+    console.log("3");
+  });
 });
